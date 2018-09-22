91eea12af3bb300b534ff54d1b8c25bd88b46647
eddanapudi <ekalyan_2000@yahoo.com>
2018-09-22 23:20:55 +0530

    dding other dependent files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {DetailsComponent} from './details/details.component';
+import {AddComponent} from './add-card/add-card.component';
+import {LoginComponent} from './login/login.component';
+import {SignupComponent} from './signup/signup.component';
+
+const routes: Routes = [
+{path: '', component: HomeComponent},
+{path: 'details/:city', component: DetailsComponent},
+{path: 'add-card', component: AddComponent},
+{path: 'login', component: LoginComponent},
+{path: 'signup', component: SignupComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
